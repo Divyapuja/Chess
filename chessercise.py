e53f865ec27b94da3fda345db153e8ce6c76df02
Divyapuja <divyapuja.vitonde@gmail.com>
2018-10-23 00:18:38 -0400

    Create chessercise.py

---
+++ chessercise.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+#Author: Divyapuja Vitonde
+#Python version: 2.7
+Def chessercise(piece, position):
+  x,y = position
+  if piece=KNIGHT:
+  elif piece=QUEEN:
+  else:
+print(listOutput)
+return
